6aed52693d4f2c9794c9c5128fe711f5a3e5ebb4
Egor Mazkov <mazko@DESKTOP-P58S1QN>
2022-11-10 15:55:32 +0300

    TelegramBot

---
+++ src/test/java/my/bot/telegrambot/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package my.bot.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
